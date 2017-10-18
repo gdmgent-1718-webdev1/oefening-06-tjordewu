3850bf2773bc37ca692f539b84e469349ae0918b
gdm-1718-tjordewu <tjordewu@student.arteveldehs.beecho>
2017-10-18 22:28:02 +0200

    first commit

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
+    
+        $currentMonth = date('m');
+        $currentMonthFull = date('F');
+        $season;
 
-function checkAge($age){
-    if($age < 16){
-        return $age . " jaar is te jong voor deze film";
-    } 
-    else if($age >= 16 && $age < 18) {
-        return $age . " jaar, je hebt ouderlijk toezicht nodig.";
-    }  
-    else{
-        return "Je bent al $age jaar. Geniet van de film!";
-    }
-}
+        if($currentMonth>="03" && $currentMonth<="05")
+            $season = "lente";
+            
+        elseif($currentMonth>="06" && $currentMonth<="08")
+            $season = "zomer";
 
-for( $i = 1; $i<19; ++$i){
-    echo checkAge($i) . "<br>";
-}
-?>
+        elseif($currentMonth>="09" && $currentMonth<="11")
+            $season = "herfst";
+            
+         else
+            $season = "winter";
+
+        echo "Het is de maand ${currentMonthFull}, dus het is ${season}";
+
+    ?>
